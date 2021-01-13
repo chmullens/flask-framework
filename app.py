7b80a95c3868eb8bccfb349a6d2627ea57e15c61
Chris Mullens <72764297+chmullens@users.noreply.github.com>
2021-01-12 21:06:45 -0700

    Starting to modify the html

--- app.py
+++ app.py
@@ -17,22 +17,21 @@ def index():
 def about():
   return render_template('about.html')
 
-@app.route('/stockplotter')
-def stockplotter:
-    return render_template('stockplotter.html')
+# @app.route('/stockplotter')
+# def stockplotter:
+#     return render_template('stockplotter.html')
 
-
-@app.route('/plot', methods=['POST'])
-def plot():
-    #This is a dictionary-like object that contains the info
-    #contained in the request. Since he called the form "my_name",
-    #you'll call that for the form.
-    first_name = request.form["my_name"]
-    full_name = create_plot(first_name)
-
-    #Here, we'd render a new plot template... which could
-    #include the rendering of a button itself.
-    return full_name
+# @app.route('/plot', methods=['POST'])
+# def plot():
+#     #This is a dictionary-like object that contains the info
+#     #contained in the request. Since he called the form "my_name",
+#     #you'll call that for the form.
+#     first_name = request.form["my_name"]
+#     full_name = create_plot(first_name)
+#
+#     #Here, we'd render a new plot template... which could
+#     #include the rendering of a button itself.
+#     return full_name
 
 
 if __name__ == '__main__':
