a51979725192b3b2e2c16c5aff2dda142b522839
Chris Mullens <72764297+chmullens@users.noreply.github.com>
2021-01-12 20:49:57 -0700

    initial testing set

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, PreText, Select
+from bokeh.plotting import figure
+
+DEFAULT_TICKERS = ['AAPL', 'GOOG', 'INTC', 'BRCM', 'YHOO']
 
 app = Flask(__name__)
 #Simple edit.
@@ -11,5 +17,27 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/plot', methods=['POST'])
+def plot():
+    #This is a dictionary-like object that contains the info
+    #contained in the request. Since he called the form "my_name",
+    #you'll call that for the form.
+    first_name = request.form["my_name"]
+    full_name = create_plot(first_name)
+
+    #Here, we'd render a new plot template... which could
+    #include the rendering of a button itself.
+    return full_name
+
 if __name__ == '__main__':
   app.run(port=33507)
+
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run():
